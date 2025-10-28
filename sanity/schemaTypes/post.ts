6ddd6108040f594fe63631ec3613f96121343fbb
DRlFTER <wwwkaweesha5@gmail.com>
2025-10-28 20:48:59 +0530

    feat: added a second page to show routing, setup a sanity based post page

---
+++ sanity/schemaTypes/post.ts
@@ -0,0 +1,29 @@
+import { defineField, defineType } from "sanity";
+
+export const post = defineType({
+  name: "post",
+  title: "Post",
+  type: "document",
+  fields: [
+    defineField({
+      name: "title",
+      type: "string",
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: "slug",
+      type: "slug",
+      options: { source: "title", maxLength: 96 },
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: "excerpt",
+      type: "text",
+      rows: 3,
+    }),
+    defineField({
+      name: "publishedAt",
+      type: "datetime",
+    }),
+  ],
+});
