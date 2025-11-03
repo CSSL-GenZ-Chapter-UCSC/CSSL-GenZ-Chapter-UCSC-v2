12dbd9ee32047d76549aff5ceffacacb0415d766
Umesh_Yapa <umeshmadhusankha19@gmail.com>
2025-11-03 21:55:14 +0530

    feat: basic structure is set to EventsSection

--- app/components/sections/EventsSection.tsx
+++ app/components/sections/EventsSection.tsx
@@ -3,7 +3,16 @@ import { Container } from "../shared/Container";
 export const EventsSection = () => {
   return (
     <Container className="relative z-10 py-32 lg:py-40">
-        <div></div>
+        <div className="bg-blue-700 min-h-[920px] ">
+            {/* timeline */}
+            <div></div>
+
+            {/* eventNames */}
+            <div></div>
+
+            {/* eventPhotos */}
+            <div></div>
+        </div>
     </Container>
     );
 };
