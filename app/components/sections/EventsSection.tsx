e6096b22cf22bebfe30246c1a8986204a63a28d9
Umesh_Yapa <umeshmadhusankha19@gmail.com>
2025-11-03 19:45:52 +0530

    feat: implement EventsSection component

---
+++ app/components/sections/EventsSection.tsx
@@ -0,0 +1,9 @@
+import { Container } from "../shared/Container";
+
+export const EventsSection = () => {
+  return (
+    <Container className="relative z-10 py-32 lg:py-40">
+        <div></div>
+    </Container>
+    );
+};
