321a068bb2e0de1023adcca99cec5250a8c41ee5
Umesh_Yapa <umeshmadhusankha19@gmail.com>
2025-11-03 21:59:41 +0530

    feat: enhancing basic structure of eventSection module

--- app/components/sections/EventsSection.tsx
+++ app/components/sections/EventsSection.tsx
@@ -3,15 +3,21 @@ import { Container } from "../shared/Container";
 export const EventsSection = () => {
   return (
     <Container className="relative z-10 py-32 lg:py-40">
-        <div className="bg-blue-700 min-h-[920px] ">
+        <div className="bg-blue-700 min-h-[920px] flex">
             {/* timeline */}
-            <div></div>
+            <div className="min-h-[920px] w-[10%] bg-black/30">
+
+            </div>
 
             {/* eventNames */}
-            <div></div>
+            <div className="min-h-[920px] w-[43%] bg-black/50">
+
+            </div>
 
             {/* eventPhotos */}
-            <div></div>
+            <div className="min-h-[920px] w-[47%] bg-black/70">
+
+            </div>
         </div>
     </Container>
     );
