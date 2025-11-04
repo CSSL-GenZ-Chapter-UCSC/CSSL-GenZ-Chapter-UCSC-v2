e4fd4740575e59e7191c437633d04406d5149b1c
Umesh_Yapa <umeshmadhusankha19@gmail.com>
2025-11-04 12:15:58 +0530

    feat:  commit before diving in to js functions

--- app/components/sections/EventsSection.tsx
+++ app/components/sections/EventsSection.tsx
@@ -5,22 +5,11 @@ export const EventsSection = () => {
     <Container className="relative z-10 py-32 lg:py-40">
         <div className="bg-blue-700 min-h-[920px] flex">
             {/* timeline and eventNames*/}
+            {/* loop should come and 1 following struction for 1 event*/}
             <div className="bg-green-400 flex items-center justify-between h-[705px] w-[53%]"> 
                 <div className="bg-amber-300 text-white block w-[18%] h-[215px]">Date 2</div>
                 <div className="bg-red-500 min-h-[215px] w-[80%] h-[215px]"></div>
             </div>
-                {/* <div className="flex flex-col justify-between items-center min-h-[920px] w-[10%] bg-black/30">
-                    <span className="text-white block">Date 1</span>
-                    <span className="text-white block">Date 2</span>
-                    <span className="text-white block">Date 3</span>
-                </div> */}
-
-            {/* eventNames */}
-            {/* <div className="flex flex-col justify-between min-h-[920px] w-[43%] bg-black/50 overflow-hidden">
-                <div className="bg-white/30 min-h-[215px] -translate-y-1/2"></div>
-                <div className="bg-white/30 min-h-[215px] mx-2"></div>
-                <div className="bg-white/30 min-h-[215px] translate-y-1/2"></div>
-            </div> */}
 
             {/* eventPhotos */}
             <div className="grid grid-rows-4 grid-cols-2 gap-1 min-h-[920px] w-[47%] bg-black/70">
