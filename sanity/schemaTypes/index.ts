c5f767a667886510f42e1f7a85bb32c153dcffdc
Sivajan-Jeyaseelan <jeyaseelansivajan@gmail.com>
2025-11-10 22:28:06 +0530

    bloglist and blog detail pages

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -2,7 +2,8 @@ import { type SchemaTypeDefinition } from 'sanity'
 import { post } from './post'
 import { blog } from './blog'
 import { event } from './event'
+import { author } from './author'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, blog, event],
+  types: [post, blog, event,author],
 }
