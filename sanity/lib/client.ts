357784d6a144fab0d39f3e42ba3fd65012bae16e
Sivajan-Jeyaseelan <jeyaseelansivajan@gmail.com>
2025-12-01 08:42:35 +0530

    solving some issue

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,9 +1,11 @@
-import { createClient } from "next-sanity";
+import { createClient } from 'next-sanity'
+
+import { apiVersion, dataset, projectId } from '../env'
 
 export const client = createClient({
-  projectId: "3aby8hgp",
-  dataset: "production",
-  apiVersion: "2023-10-01",
-  useCdn: false,
-  perspective: "published",
-});
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: false, // Set to false for dynamic pages to ensure fresh data
+  perspective: 'published', // Only fetch published documents
+})
