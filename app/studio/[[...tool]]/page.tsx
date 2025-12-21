529690b5ac05c7ed7e15793787264aa62d48d836
DRlFTER <wwwkaweesha5@gmail.com>
2025-12-21 20:39:00 +0530

    fix: sanity error

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -2,17 +2,7 @@
 
 import { NextStudio } from "next-sanity/studio";
 import config from "../../../sanity.config";
-import { useEffect, useState } from "react";
 
 export default function StudioPage() {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => setMounted(true), 0);
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (!mounted) return null;
-
   return <NextStudio config={config} />;
 }
